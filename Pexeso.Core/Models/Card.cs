878be8cfebb4a9f1bd974890f12a87a6b728e46f
stepanklein <stepan.klein@verisoft.cz>
2024-06-26 21:52:51 +0200

    ahoj

---
+++ Pexeso.Core/Models/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pexeso.Core.Models;
+
+public class Card
+{
+    public string Name { get; set; }
+
+
+}
---
+++ Pexeso.Core/Models/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace Pexeso.Core.Models;
+
+public class Player
+{
+    public string Name { get; set; } = string.Empty;
+
+    public List<Card> Cards { get; set; } = new();
+}
